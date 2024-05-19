72a35799614e2576468eeb70d84004837d472be9
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-19 18:52:19 +0530

    cursor done

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+function cursor() {
+    window.addEventListener("mousemove", function(dets) {
+        document.querySelector("#cursor").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    });
+}
+cursor();
