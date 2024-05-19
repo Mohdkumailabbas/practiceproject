dd9b113d7b52b6ef772108196b8df491b25ae700
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-19 22:34:47 +0530

    done

--- script.js
+++ script.js
@@ -4,9 +4,43 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function firstpage() {
+    var tl = gsap.timeline();
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: "expo.out"
+    });
+     tl.to("#elem",{
+        y:0,
+        duration:0.5,
+        ease: "expo.out"
+     })
+     tl.to("#hey",{
+        y:0,
+        duration:0.5,
+        ease: "expo.out"
+     })
+     tl.to("#avail",{
+        y:0,
+        duration:0.5,
+        ease: "expo.out"
+     })
+     .from("#fotter1", {
+        y: '-10',
+        opacity: 0,
+        duration: 0.5,
+        ease: "expo.out",
+        delay:0.5
+    });
+}
+
 function cursor() {
     window.addEventListener("mousemove", function(dets) {
         document.querySelector("#cursor").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
     });
 }
 cursor();
+firstpage();
